57b12541e869e418c9134289be6d6e910238eb99
Alexander <101837359+APshellxx@users.noreply.github.com>
2026-03-23 09:46:20 +0100

    New server with bun
    We changed the entire system and how it works, this is gonna take a while...

--- Server/index.ts
+++ Server/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import { serve } from "bun"; 
+
+console.log("Server is running on http://localhost:3000");
+console.log("Restarted at:", Date.now());
+
+const website = Bun.file("./index.html");
+
+serve({
+    port: 3000,
+    fetch(req) {
+        return new Response(website);
+    },
+});
